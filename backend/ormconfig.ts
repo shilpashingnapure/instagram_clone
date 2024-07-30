684606a6b2352271cf8378f64dc12a4e2e294edc
shilpashingnapure <shingnapureshilpa2@gmail.com>
2024-07-30 20:27:14 +0530

    speeling mistake

--- backend/ormconfig.ts
+++ backend/ormconfig.ts
@@ -9,7 +9,7 @@ config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_TYPE,
+  host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT || "5432"),
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
