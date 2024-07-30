96719f7b956669a968f34daca9bb30a63488662d
shilpashingnapure <shingnapureshilpa2@gmail.com>
2024-07-30 21:27:47 +0530

    added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -27,7 +27,7 @@ async function startServer() {
   app.use(
     cors({
       credentials: true, // Allow cookies to be sent
-      origin: ["http://localhost:3000" , "https://photogramm.vercel.app"],
+      origin: ["*"],
     })
   );
   app.use(cookieParser());
