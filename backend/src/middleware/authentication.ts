6466a6064d2655642c06d6cd582d0c36ff1dc70b
shilpashingnapure <shingnapureshilpa2@gmail.com>
2024-07-30 22:13:57 +0530

    cors

--- backend/src/middleware/authentication.ts
+++ backend/src/middleware/authentication.ts
@@ -10,7 +10,8 @@ export interface CustomRequest extends Request {
 
 export function authenticate(req : CustomRequest  , res : Response  , next : NextFunction ){
     const token = req.cookies.token
-    console.log(req.cookies);
+    console.log('Request Cookies:', req.cookies); // This might show [Object: null prototype] {}
+    console.log('Request Headers:', req.headers);
     if(!token){
         return res.status(401).json({ message : 'No token provided'});
     }
